c0af744706c31278cd9c47ca4cf7b83409b98f41
Budaházi Bence <bence0630@gmail.com>
2018-11-12 16:40:19 +0100

    Szülséges osztályok hozzáadása

---
+++ ConsoleAppInterfacek/Dvd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInterfacek
+{
+    class Dvd
+    {
+        private string cim;
+        private int hossz; //perc
+
+        public Dvd(string cim, int hossz)
+        {
+            this.cim = cim;
+            this.hossz = hossz;
+        }
+
+        public string Cim
+        {
+            get
+            {
+                return cim;
+            }
+
+            set
+            {
+                cim = value;
+            }
+        }
+
+        public int Hossz
+        {
+            get
+            {
+                return hossz;
+            }
+
+            set
+            {
+                hossz = value;
+            }
+        }
+
+        public double TeljesHosszOra()
+        {
+            return this.hossz / 60.0;
+        }
+    }
+}
---
+++ ConsoleAppInterfacek/Ujsag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInterfacek
+{
+    class Ujsag
+    {
+        string cim;
+        int kiadasEv;
+        int kiadasHonap;
+
+        public Ujsag(string cim, int kiadasEv, int kiadasHonap)
+        {
+            this.cim = cim;
+            this.kiadasEv = kiadasEv;
+            this.kiadasHonap = kiadasHonap;
+        }
+    }
+}
