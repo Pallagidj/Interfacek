95b23ba53ff36fa892910ba147b4a82b1e0012c3
Budaházi Bence <bence0630@gmail.com>
2018-11-12 16:56:18 +0100

    Program előkíszétése hivatkozás bemutatására.

--- ConsoleAppInterfacek/Program.cs
+++ ConsoleAppInterfacek/Program.cs
@@ -8,8 +8,49 @@ namespace ConsoleAppInterfacek
 {
     class Program
     {
+        int szam;
+        string szoveg;
+        int[] tomb;
+
+
         static void Main(string[] args)
         {
+            Program p1 = new Program();
+            Console.WriteLine(p1.szam);
+            Console.WriteLine(p1.szoveg);
+            Console.WriteLine(p1.tomb);
+
+
+            Dvd d1 = new Dvd("Star Wars IV", 180);
+            Dvd d2 = new Dvd("Star Wars V", 230);
+            Dvd d3 = new Dvd("Star Wars VI", 210);
+
+            var katalogus = new List<object>();
+            katalogus.Add(d1);
+            katalogus.Add(d2);
+            katalogus.Add(d3);
+            katalogus.Add(new Konyv("Harry Potter és a Bölcsek köve", "J.K. Rowling", "963-8386-77-0"));
+            katalogus.Add(new Ujsag("Blikk",2016,3));
+
+            var dvdk = new List<Dvd>();
+
+            dvdk.Add(d1);
+            dvdk.Add(d2);
+            dvdk.Add(d3);
+
+            foreach (var item in dvdk)
+            {
+                Console.WriteLine(item.Cim);
+            }
+
+            d2.Cim = "Star Wars II";
+
+            foreach (var item in dvdk)
+            {
+                Console.WriteLine(item.Cim);
+            }
+
+            Console.ReadLine();
         }
     }
 }
