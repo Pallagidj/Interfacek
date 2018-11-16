8251d2681dd7d5b35ff4cc0df282c0dbb9ce27ba
Petrik Lajos Szg <info@petrik.hu>
2018-11-16 07:52:30 +0100

    Hivatkozások bemutatása.

--- ConsoleAppInterfacek/Program.cs
+++ ConsoleAppInterfacek/Program.cs
@@ -8,29 +8,21 @@ namespace ConsoleAppInterfacek
 {
     class Program
     {
-        int szam;
-        string szoveg;
-        int[] tomb;
-
-
+       
         static void Main(string[] args)
         {
-            Program p1 = new Program();
-            Console.WriteLine(p1.szam);
-            Console.WriteLine(p1.szoveg);
-            Console.WriteLine(p1.tomb);
-
-
             Dvd d1 = new Dvd("Star Wars IV", 180);
             Dvd d2 = new Dvd("Star Wars V", 230);
             Dvd d3 = new Dvd("Star Wars VI", 210);
-
+            
             var katalogus = new List<object>();
             katalogus.Add(d1);
             katalogus.Add(d2);
             katalogus.Add(d3);
             katalogus.Add(new Konyv("Harry Potter és a Bölcsek köve", "J.K. Rowling", "963-8386-77-0"));
             katalogus.Add(new Ujsag("Blikk",2016,3));
+            
+            
 
             var dvdk = new List<Dvd>();
 
@@ -43,7 +35,12 @@ namespace ConsoleAppInterfacek
                 Console.WriteLine(item.Cim);
             }
 
+            Dvd d4 = d2;
+            Console.WriteLine(d4.Cim);
+
             d2.Cim = "Star Wars II";
+
+            Console.WriteLine(d4.Cim);
 
             foreach (var item in dvdk)
             {
